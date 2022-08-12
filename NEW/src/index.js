1d84f53bbb46fe2196393fdea4a0703204bf94bd
Priyanshu Rajput <priyanshu.21march@gmail.com>
2022-08-12 13:42:54 +0530

    Add files via upload

---
+++ NEW/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MoralisProvider appId="az6aHVFbfkfcW50HF37zbU7rWRsPVllX975hHoKy"
+      serverUrl="https://jvrqac43agsp.usemoralis.com:2053/server">
+      <NotificationProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
